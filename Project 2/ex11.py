053fb4111e8c11d6efdbc87fb9662c654eb1915d
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-06-03 03:17:11 +0200

    ex1 done

--- Project 2/ex11.py
+++ Project 2/ex11.py
@@ -88,43 +88,32 @@ a2 = np.abs(np.fft.ifft2(R2))
 a3 = np.abs(np.fft.ifft2(R3))
 A = cv2.merge((a1, a2, a3))
 
-
-# o1 = np.fft.fftshift(np.fft.fft2(k[:, :, 0]))
-# o2 =np.fft.fftshift(np.fft.fft2(k[:, :, 1]))
-# o3 =np.fft.fftshift(np.fft.fft2(k[:, :, 2]))
 nnB = img - l
-xx =np.fft.fftshift(np.fft.fft2(nnB))
-snnB1 = abs(xx) ** 2
-# snnB1 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 0]))) ** 2
-# snnB2 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 1]))) ** 2
-# snnB3 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 2]))) ** 2
-F = np.fft.fftshift(np.fft.fft2(img))
-sxxB1 = abs(F) ** 2
-qq = np.fft.fftshift(np.fft.fft2(l))
-# sxxB2 = abs(F2) ** 2
-# sxxB3 = abs(F3) ** 2
-IB1 = qq / xx
-# IB2 = q2 / F2
-# IB3 = q3 / F3
-K = np.mean((snnB1 / sxxB1))
-dhB1 = np.abs(IB1) ** 2 + K
-# dhB2 = np.abs(IB2) ** 2 + (snnB2 / sxx2)
-# dhB3 = np.abs(IB3) ** 2 + (snnB3 / sxx3)
-HwB1 =  np.conj(IB1)  / dhB1
-
-
-# HwB2 = np.conj(IB2) / dhB2
-# HwB3 = np.conj(IB3) / dhB3
-RB1 = HwB1[:,:,0] * G1
-RB2 = HwB1[:,:,1] * G2
-RB3 = HwB1[:,:,2] * G3
+snnB1 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 0]))) ** 2
+snnB2 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 1]))) ** 2
+snnB3 = abs(np.fft.fftshift(np.fft.fft2(nnB[:, :, 2]))) ** 2
+IB1 = q1 / F1
+IB2 = q2 / F2
+IB3 = q3 / F3
+K1 = np.mean((sxx1/snnB1))
+K2 = np.mean((sxx2/snnB2))
+K3 = np.mean((sxx3/snnB3))
+dhB1 = np.abs(IB1) ** 2 + (K1)
+dhB2 = np.abs(IB2) ** 2 + (K2)
+dhB3 = np.abs(IB3) ** 2 + (K3)
+HwB1 = np.conj(IB1) / dhB1
+HwB2 = np.conj(IB2) / dhB2
+HwB3 = np.conj(IB3) / dhB3
+RB1 = HwB1 * G1
+RB2 = HwB2 * G2
+RB3 = HwB3 * G3
 aB1 = np.abs(np.fft.ifft2(RB1))
 aB2 = np.abs(np.fft.ifft2(RB2))
 aB3 = np.abs(np.fft.ifft2(RB3))
 AB = cv2.merge((aB1, aB2, aB3))
 
 
-cv2.imshow("blue", g)
+cv2.imshow("blur", g)
 cv2.imshow("deblue", d/np.max(d))
 cv2.imshow("denoise", P/np.max(P))
 cv2.imshow("noise&blur", l/np.max(l))
